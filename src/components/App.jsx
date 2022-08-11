39df06062d45719957fbbffd02558f9992873ee0
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-08-11 10:38:37 +0300

    xxx

---
+++ src/components/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from 'pages/Home';
+import { About } from 'pages/About';
+import { Products } from 'pages/Products';
+import { ProductDetails } from 'pages/ProductDetails';
+import { NotFound } from 'pages/NotFound';
+import { Team } from './Team';
+import { Reviews } from './Reviews';
+import { Mission } from './Mission';
+import { SharedLayout } from './SharedLayout';
+
+// import { Container, Header, Logo, Link } from './App.styled';
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />;
+      </Route>
+    </Routes>
+  );
+};
